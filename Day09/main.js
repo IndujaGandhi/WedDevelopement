3b02302de80b04a4093a9d3fa38893b557cbcb80
Induja Gandhi <indujag2202@gmail.com>
2026-07-17 11:53:20 +0530

    Updated Day09 work JS import & export

---
+++ Day09/main.js
@@ -0,0 +1,5 @@
+// main.js (another file)
+import { number, add } from "./math.js";
+
+console.log(number);      // 10
+console.log(add(5, 3));   // 8
---
+++ Day09/math.js
@@ -0,0 +1,6 @@
+// math.js (module file)
+export const number = 10;
+
+export function add(a, b) {
+  return a + b;
+}
