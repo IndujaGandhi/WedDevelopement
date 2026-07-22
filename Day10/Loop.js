95680af560db5f1b10cafa8f479507556a4988ac
Induja Gandhi <indujag2202@gmail.com>
2026-07-22 11:18:00 +0530

    Updated Day 10

---
+++ Day10/Loop.js
@@ -0,0 +1,18 @@
+console.log("For loop");
+for(let j=0; j<=10;j++){
+    console.log(j);
+}
+
+console.log("\n While loop");
+let i = 0;
+while(i<=10){
+    console.log(i+1);
+    i++;
+}
+
+console.log("\n Do while loop");
+let n=1;
+do{
+    console.log(n);
+    n++;
+}while(n<=3);
